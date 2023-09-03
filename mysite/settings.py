5b7c4fe9a1fd105f13388913c62a211b88ba7dae
Amr Khaled <lewolfje@gmail.com>
2023-09-03 19:55:41 +0300

    change from sqllite db to postgre db

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_user_password',
+        'HOST': 'localhost',  # Specify the host where your PostgreSQL server is running
+        'PORT': '5432',  # The default PostgreSQL port
     }
 }
 
